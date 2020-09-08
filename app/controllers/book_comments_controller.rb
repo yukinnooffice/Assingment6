52137462d058b7bb1a99eaad26e7746d311e6fdf
vagrant <vagrant@localhost.localdomain>
2020-09-08 11:48:29 +0000

    first commit

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,25 +3,25 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @book_comment = current_user.book_comments.new(book_comment_params)
-    @book_comment.book_id = @book.id
-    if @book_comment.save
-      flash[:success] = "Comment was successfully created."
-    end
-    redirect_to request.referer
+    @book_comment = @book.book_comments.build(book_comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comments = @book.book_comments
+    @book_comment.save
+    flash[:success] = "Comment was successfully created."
   end
 
   def destroy
     @book = Book.find(params[:book_id])
+    @book_comments = @book.book_comments
     book_comment = current_user.book_comments.find_by(id: params[:id], book_id: @book.id)
+
     book_comment.destroy
-    redirect_to request.referer
   end
 
   private
 
   def book_comment_params
-    params.require(:book_comment).permit(:comment)
+    params.require(:book_comment).permit(:comment,:book_id,:user_id)
   end
 
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,19 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :favorite_params
   def create
-    @book = Book.find(params[:book_id])
+
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
+
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_to request.referer
+  end
+
+    def favorite_params
+    @book = Book.find(params[:book_id])
   end
 end
